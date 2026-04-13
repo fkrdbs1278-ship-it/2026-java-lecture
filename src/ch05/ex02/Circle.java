d21ca9a43950f4635e6d7bc2a7e167ce31aff988
kim424 <thffhahs0424@naver.com>
2026-04-13 17:19:14 +0900

    Class 상속

--- src/ch05/ex02/Circle.java
+++ src/ch05/ex02/Circle.java
@@ -1,12 +1,10 @@
 package ch05.ex02;
 
 public class Circle extends Shape{
-
-    @Override
     double pi;
     double radius;
     void area(){
         double area = radius*radius*pi;
-        System.out.println(area);
+        System.out.println("원의 넓이는: "+area);
     }
 }
--- src/ch05/ex02/ShapeTest.java
+++ src/ch05/ex02/ShapeTest.java
@@ -1,4 +1,15 @@
 package ch05.ex02;
 
 public class ShapeTest {
+    static void main() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.hight = 10;
+        rectangle.width = 25;
+        rectangle.area();
+
+        Circle circle = new Circle();
+        circle.radius = 5;
+        circle.pi=3.14;
+        circle.area();
+    }
 }
